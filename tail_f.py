22c69514db3b65700d03d201be1b6ae8d123ed3d
maertens.a <am@rcs.de>
2018-11-12 15:29:07 +0100

    add tailf for python

---
+++ tail_f.py
@@ -0,0 +1,11 @@
+import time
+import os
+
+def follow(thefile):
+    thefile.seek(0, os.SEEK_END)
+    while True:
+        line = thefile.readline()
+        if not line:
+            time.sleep(0.1)
+            continue
+        yield line
