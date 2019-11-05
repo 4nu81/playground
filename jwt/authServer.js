0f5b1808dd9a4f3be59785d78126ce9900af61f6
maertens.a <am@rcs.de>
2019-11-05 01:03:39 +0100

    refresh jwt in node

--- jwt/authServer.js
+++ jwt/authServer.js
@@ -7,26 +7,38 @@ const jwt = require('jsonwebtoken')
 
 app.use(express.json())
 
+let refreshTokens = []
+
+app.delete('/logout', (req, res) => {
+    refreshTokens = refreshTokens.filter(token => token !== req.body.token)
+    res.sendStatus(204)
+})
+
+app.post('/token', (req, res) => {
+    const refreshToken = req.body.token
+    if (refreshToken == null) return res.sendStatus(401)
+    if (!refreshTokens.includes(refreshToken)) return res.sendStatus(403)
+    jwt.verify(refreshToken, process.env.REFRESH_TOKEN_SECRET, (err, user) => {
+        if (err) return res.sendStatus(403)
+        const accessToken = generateAccessToken({name: user.name})
+        res.json({accessToken: accessToken})
+    })
+})
+
 app.post('/login', (req, res) => {
     // Authenticate User
 
     const username = req.body.username
     const user = {name: username}
 
-    const accessToken = jwt.sign(user, process.env.ACCESS_TOKEN_SECRET)
-    res.json({acressToken: accessToken})
+    const accessToken = generateAccessToken(user)
+    const refreshToken = jwt.sign(user, process.env.REFRESH_TOKEN_SECRET)
+    refreshTokens.push(refreshToken)
+    res.json({acressToken: accessToken, refreshToken: refreshToken})
 })
 
-function authenticateToken(req, res, next) {
-    const authHeader = req.headers['authorization']
-    const token = authHeader && authHeader.split(' ')[1]
-    if (token == null) return res.sendStatus(401)
-    
-    jwt.verify(token, process.env.ACCESS_TOKEN_SECRET, (err, user)=>{
-        if (err) return res.sendStatus(403)
-        req.user = user
-        next()
-    })
+function generateAccessToken(user) {
+    return jwt.sign(user, process.env.ACCESS_TOKEN_SECRET, {expiresIn: '15s' })
 }
 
 app.listen(4000)
